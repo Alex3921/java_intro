502a30fd9dbaf1b372a9b8ba9a4a2a0bf1441f9b
Alexandru Boncut <boncut.alex10@gmail.com>
2023-01-07 19:26:45 -0600

    Add Hw14

--- src/homeworks/Homework14.java
+++ src/homeworks/Homework14.java
@@ -5,111 +5,6 @@ import java.util.List;
 
 public class Homework14 {
     public static void main(String[] args) {
-//        Requirement:
-//        -Create a method called fizzBuzz1()
-//                -This method will take an int argument, and it will print
-//        numbers starting from 1 to given argument. BUT, it will print
-//        “Fizz” for the numbers divided by 3, “Buzz” for the numbers
-//        divided by 5, and “FizzBuzz” for the numbers divided both by 3
-//        and 5.
-//        Test Data 1: 3
-//Expected Result 1:
-//1
-//2
-//Fizz
-//Test Data 2: 5
-//Expected Result 2:
-//1
-//2
-//Fizz
-//4
-//Buzz
-//Test Data 3: 18
-//Expected Result 3:
-//1
-//2
-//Fizz
-//4
-//Buzz
-//Fizz
-//7
-//8
-//Fizz
-//Buzz
-//11
-//Fizz
-//13
-//14
-//FizzBuzz
-//16
-//17
-//        Task-2
-//        Requirement:
-//        -Create a method called fizzBuzz2()
-//                -This method will take an int argument, and it will return
-//                a String. BUT it will return “Fizz” if the given number is
-//        divided by 3, “Buzz” if the number is divided by 5, and
-//“FizzBuzz” if the number is divided both by 3 and 5.
-//        Otherwise, it will return number itself.
-//                Test Data 1: 0
-//        Expected Result 1: FizzBuzz
-//        Test Data 2: 1
-//        Expected Result 2: 1
-//        Test Data 3: 3
-//        Expected Result 3: Fizz
-//        Test Data 4: 5
-//        Expected Result 4: Buzz
-//        Test Data 5: 15
-//        Expected Result 5: FizzBuzz
-//        Task-3
-//        Requirement:
-//        -Create a method called findSumNumbers()
-//                -This method will take a String argument and it will return
-//                an int which is the sum of all numbers presented in the String.
-//        NOTE: If there are no numbers represented in the String,
-//        return 0.
-//        Test Data 1: “abc$”
-//        Expected Result 1: 0
-//        Test Data 2: “a1b4c  6#”
-//        Expected Result 2: 11
-//        Test Data 3: “ab110c045d”
-//        Expected Result 3: 155
-//        Test Data 4: “525”
-//        Expected Result 4: 525
-//        Task-4
-//        Requirement:
-//        -Create a method called findBiggestNumber()
-//                -This method will take a String argument and it will
-//        return an int which is the number presented in the
-//        String.
-//                NOTE: If there are no numbers represented in the String,
-//        return 0.
-//        Test Data 1: “abc$”
-//        Expected Result 1: 0
-//        Test Data 2: “a1b4c  6#”
-//        Expected Result 2: 6
-//        Test Data 3: “ab110c045d”
-//        Expected Result 3: 110
-//        Test Data 4: “525”
-//        Expected Result 4: 525
-//        Task-5
-//        Requirement:
-//        -Create a method called
-//        countSequenceOfCharacters()
-//                -This method will take a String argument and it will
-//        return a String which is the count of repeated
-//        characters in a sequence in the String.
-//                NOTE: If given String is empty, then return empty String.
-//                NOTE: It is case sensitive!!!
-//                Test Data 1: “”
-//        Expected Result 1: “”
-//        Test Data 2: “abc”
-//        Expected Result 2: “1a1b1c”
-//        Test Data 3: “abbcca”
-//        Expected Result 3: “1a2b2ca”
-//        Test Data 4: “aaAAa”
-//        Expected Result 4: “2a2A1a”
-
         System.out.println("\n------------------------fizzBuzz1------------------------\n");
 
         // Test case 1
@@ -169,28 +64,28 @@ public class Homework14 {
 
         System.out.println("\n------------------------findSumNumbers------------------------\n");
 
-// Test case 1
+        // Test case 1
         String findSumNumbersTest1 = "abc$";
         System.out.println("Test data 1: " + findSumNumbersTest1);
         System.out.println("Expected output: 0");
         System.out.println("Actual output: " + findSumNumbers(findSumNumbersTest1));
         System.out.println();
 
-// Test case 2
+        // Test case 2
         String findSumNumbersTest2 = "a1b4c  6#";
         System.out.println("Test data 2: " + findSumNumbersTest2);
         System.out.println("Expected output: 11");
         System.out.println("Actual output: " + findSumNumbers(findSumNumbersTest2));
         System.out.println();
 
-// Test case 3
+        // Test case 3
         String findSumNumbersTest3 = "ab110c045d";
         System.out.println("Test data 3: " + findSumNumbersTest3);
         System.out.println("Expected output: 155");
         System.out.println("Actual output: " + findSumNumbers(findSumNumbersTest3));
         System.out.println();
 
-// Test case 4
+        // Test case 4
         String findSumNumbersTest4 = "525";
         System.out.println("Test data 4: " + findSumNumbersTest4);
         System.out.println("Expected output: 525");
@@ -199,28 +94,28 @@ public class Homework14 {
 
         System.out.println("\n------------------------findBiggestNumber------------------------\n");
 
-// Test case 1
+        // Test case 1
         String findBiggestNumberTest1 = "abc$";
         System.out.println("Test data 1: " + findBiggestNumberTest1);
         System.out.println("Expected output: 0");
         System.out.println("Actual output: " + findBiggestNumber(findBiggestNumberTest1));
         System.out.println();
 
-// Test case 2
+        // Test case 2
         String findBiggestNumberTest2 = "a1b4c  6#";
         System.out.println("Test data 2: " + findBiggestNumberTest2);
         System.out.println("Expected output: 6");
         System.out.println("Actual output: " + findBiggestNumber(findBiggestNumberTest2));
         System.out.println();
 
-// Test case 3
+        // Test case 3
         String findBiggestNumberTest3 = "ab110c045d";
         System.out.println("Test data 3: " + findBiggestNumberTest3);
         System.out.println("Expected output: 110");
         System.out.println("Actual output: " + findBiggestNumber(findBiggestNumberTest3));
         System.out.println();
 
-// Test case 4
+        // Test case 4
         String findBiggestNumberTest4 = "525";
         System.out.println("Test data 4: " + findBiggestNumberTest4);
         System.out.println("Expected output: 525");
@@ -229,28 +124,28 @@ public class Homework14 {
 
         System.out.println("\n------------------------countSequenceOfCharacters------------------------\n");
 
-// Test case 1
+        // Test case 1
         String countSequenceOfCharactersTest1 = "";
         System.out.println("Test data 1: " + countSequenceOfCharactersTest1);
         System.out.println("Expected output: \"\"");
         System.out.println("Actual output: \"" + countSequenceOfCharacters(countSequenceOfCharactersTest1) + "\"");
         System.out.println();
 
-// Test case 2
+        // Test case 2
         String countSequenceOfCharactersTest2 = "abc";
         System.out.println("Test data 2: " + countSequenceOfCharactersTest2);
         System.out.println("Expected output: \"1a1b1c\"");
         System.out.println("Actual output: \"" + countSequenceOfCharacters(countSequenceOfCharactersTest2) + "\"");
         System.out.println();
 
-// Test case 3
+        // Test case 3
         String countSequenceOfCharactersTest3 = "abbcca";
         System.out.println("Test data 3: " + countSequenceOfCharactersTest3);
         System.out.println("Expected output: \"1a2b2c1a\"");
         System.out.println("Actual output: \"" + countSequenceOfCharacters(countSequenceOfCharactersTest3) + "\"");
         System.out.println();
 
-// Test case 4
+        // Test case 4
         String countSequenceOfCharactersTest4 = "aaAAa";
         System.out.println("Test data 4: " + countSequenceOfCharactersTest4);
         System.out.println("Expected output: \"2a2A1a\"");
